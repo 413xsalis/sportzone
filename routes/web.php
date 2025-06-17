d1ed6cf77cf699bffba545766e5f9525a6b0717a
Leidi11 <leidyflorez2002@gmail.com>
2025-06-17 13:45:48 -0500

    Volviendo al incio

--- routes/web.php
+++ routes/web.php
@@ -24,28 +24,4 @@ Route::prefix('admin')->group(function () {
     Route::get('/formulario', [AdminController::class, 'formulario'])->name('admin.Formulario_empleados');
 });
 
-//Grupo Instructor
-Route::prefix('instructor/inicio')->name('instructor.inicio.')->group(function () {
-    Route::get('/principal', [InstructorController::class, 'principal'])->name('principal');
-});
 
-
-Route::prefix('instructor/formulario')
-    ->name('instructor.formulario.')
-    ->group(function () {
-        // 1) Listado de estudiantes (GET  /instructor/formulario)
-        Route::get('/', [FormularioController::class, 'index'])
-            ->name('principal');
-
-        // 2) Mostrar formulario de creación (GET  /instructor/formulario/crear)
-        Route::get('crear', [FormularioController::class, 'crearEstudiante'])
-            ->name('crear');
-
-        // 3) Guardar nuevo estudiante (POST /instructor/formulario)
-        Route::post('/', [FormularioController::class, 'guardarEstudiante'])
-            ->name('guardar');
-
-        // 4) Actualizar asistencia (POST /instructor/formulario/asistencia/{estudiante})
-        Route::post('asistencia/{estudiante}', [FormularioController::class, 'asistencia'])
-            ->name('asistencia.update');
-    });
